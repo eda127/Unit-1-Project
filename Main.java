76b4df827f5556ba9dc96402f17719e2f193d460
Eclipse-V <christinevanson77@gmail.com>
2024-09-13 14:17:27 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Give a last name: ");
+        String lastName = input.nextLine();
+        System.out.println("Give an adjective to describe animals: ");
+        String adjAnimals = input.nextLine();
+        System.out.println("Give a random number: ");
+        int num = input.nextInt();
+        System.out.println("Give a random animal: ");
+        String animal = input.nextLine();
+        System.out.println("True or False? ");
+        boolean isTigers = input.nextBoolean();        
         
     }
 }
